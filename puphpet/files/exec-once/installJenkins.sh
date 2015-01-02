185dc6cb94ce87c46dc3b824f4b132d4d3961e4f
Eric Hogue <eric@erichogue.ca>
2015-01-01 22:45:06 -0500

    Added Github plugin

--- puphpet/files/exec-once/installJenkins.sh
+++ puphpet/files/exec-once/installJenkins.sh
@@ -15,7 +15,7 @@ echo "Install Jenkins plugins"
 curl  -L http://updates.jenkins-ci.org/update-center.json | sed '1d;$d' | curl -X POST -H 'Accept: application/json' -d @- http://localhost:8080/updateCenter/byId/default/postBack
 
 wget http://localhost:8080/jnlpJars/jenkins-cli.jar
-java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin Git Phing GreenBalls ChuckNorris
+java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin Git GitHub Phing GreenBalls ChuckNorris
 
 
 echo "Copy Jenkins configs"
